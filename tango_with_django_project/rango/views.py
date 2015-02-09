10680e48e8ef2f2e46502ce30c82e64062ecf04f
2089488 <2089488@gla.ac.uk>
2015-02-09 16:14:17 +0000

    chapter 5, added rel-path to static, rango.jpg, rel_path to templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Rango says "Hey there world!"<p>'
-                        '<a href="/rango/about">about</a>')
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return HttpResponse('Rango says: '
