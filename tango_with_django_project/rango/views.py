99a889532ba3ef96fac9ed29b4aac648d53f4831
2089488 <2089488@gla.ac.uk>
2015-02-09 03:08:18 +0000

    Chapter 4 completed. Added views.py, url mappings for index and about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse('Rango says "Hey there world!"<p>'
+                        '<a href="/rango/about">about</a>')
+
+def about(request):
+    return HttpResponse('Rango says: '
+                        '<br>"This is the <b>about</b> page<p>'
+                        '<a href="/rango">Index</a>')
