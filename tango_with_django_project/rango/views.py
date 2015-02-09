6e9d9474efda4d9e7ea8967c201ad18253ac40a0
2089488 <2089488@gla.ac.uk>
2015-02-09 20:01:24 +0000

    chapter 5, added rango.jpg to index page, myPic to about page using templtes and static sources, about.html template created, nedia server created

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse('Rango says: '
-                        '<br>"This is the <b>about</b> page<p>'
-                        '<a href="/rango">Index</a>')
+    context_dict = {'displayedBold': "about"}
+    return render(request, 'rango/about.html', context_dict)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -80,14 +80,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_PATH = os.path.join(BASE_DIR, "static")
-
 STATIC_URL = '/static/'
-
+STATIC_PATH = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
