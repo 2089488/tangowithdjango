db4feadc6bf51155ba2ca76fb2037a4b556bdfcb
2089488 <2089488@gla.ac.uk>
2015-02-10 06:49:20 +0000

    2089488 chapter 7 dbase repopulated and remigrated

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+        name = models.CharField(max_length=128, unique=True)
+        views = models.IntegerField(default=0)
+        likes = models.IntegerField(default=0)
 
-    def __unicode__(self):
-        return self.name
+        def __unicode__(self):
+                return self.name
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
