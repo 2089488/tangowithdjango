3ca9d410f6410163786f5fd290a9d9eff3a3fa1f
2089488 <2089488@gla.ac.uk>
2015-02-21 19:45:24 +0000

    Chapter 7 completed. Top5 list of pages. Works as expected. Problem - accessing fields when using template's filters

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,51 +12,61 @@ def populate():
 
     add_page(cat=me_cat,
              title="GitHub",
-             url="https://github.com/2089488/tangowithdjango")
+             url="https://github.com/2089488/tangowithdjango",
+             views=122)
 
     add_page(cat=me_cat,
              title="pythonanywhere",
-             url="https://www.pythonanywhere.com/user/2089488/consoles/")
+             url="https://www.pythonanywhere.com/user/2089488/consoles/",
+             views=12)
 
     python_cat = add_cat('Python', 128, 64)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=33)
 
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=25)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=79)
 
     django_cat = add_cat("Django", 64, 32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=41)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=17)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=68)
 
     frame_cat = add_cat("Other Frameworks", 32, 16)
 
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=19)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=34)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
