4205186b0590852e76573a5762bd9c73f308f4e4
2089488 <2089488@gla.ac.uk>
2015-03-02 17:19:09 +0000

    cleans project

--- tango_with_django_project/alphabet/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from alphabet import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.abc, name='spelling'),
-                       url(r'^reverse/', views.reverse, name='reverse_alphabet'))
